a9215cfe4c2cd2bfeac41e128e624d266b0cc5fe
Mooncake Sugar <Tsukina7mochi@gmail.com>
2025-10-30 18:09:47 +0900

    feat: update source commit hash

--- definitions/init.lua
+++ definitions/init.lua
@@ -1,3 +1,3 @@
 ---@meta
 
---based on https://github.com/aseprite/api.git 7289b62da9b4a33732ce3a2907cd518890414b7f
+--based on https://github.com/aseprite/api.git 015529ac0b33177a3cb3b331ddc56c841fa22645
